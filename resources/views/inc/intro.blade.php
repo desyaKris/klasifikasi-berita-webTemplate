0c3c7c790916da6f999026e65e2db34c124e238b
Desya <desya.kristian@gmail.com>
2018-11-29 03:01:32 +0700

    website pengenalan pola

---
+++ resources/views/inc/intro.blade.php
@@ -0,0 +1,13 @@
+<section id="intro" class="intro">
+
+    <div class="slogan">
+      <h2>SELAMAT DATANG <span class="text_color"> DI WEBSITE PREDIKSI KAMI</span> </h2>
+      <h4>KAMI MEMBANTU ANDA UNTUK MEMPREDIKSI KATEGORI BERITA</h4>
+    </div>
+    <div class="page-scroll">
+      <a href="#service" class="btn btn-circle">
+				<i class="fa fa-angle-double-down animated"></i>
+			</a>
+    </div>
+  </section>
+  
